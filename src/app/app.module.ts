be56de8deff4418cfedef09c618034c1a4f55577
likai <likai12@baidu.com>
2018-03-06 20:41:11 +0800

    ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AlertModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { TimeConvertComponent } from './modules/timeconvert/time_convert.component';
@@ -13,8 +16,12 @@ import { TimeConvertComponent } from './modules/timeconvert/time_convert.compone
     TimeConvertComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [TimeConvertComponent]
